ca178ee6a35e562430e22fa219e3c2904feb5067
Leonardo Hentschke <leo.hentschke@hotmail.com>
2025-06-14 13:44:03 -0300

    reset migrations

--- prisma/migrations/20250614164306_initial_migration/migration.sql
+++ prisma/migrations/20250614164306_initial_migration/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "AthletesRights" (
+CREATE TABLE "athletes_rights" (
     "id" SERIAL NOT NULL,
     "name" TEXT,
     "phone" TEXT,
@@ -12,20 +12,23 @@ CREATE TABLE "AthletesRights" (
     "utm_campaign" TEXT,
     "utm_content" TEXT,
     "utm_term" TEXT,
+    "clicked_whatsapp_button" BOOLEAN,
+    "webhook_sent" BOOLEAN,
+    "ip" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "AthletesRights_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "athletes_rights_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Retired" (
+CREATE TABLE "retired" (
     "id" SERIAL NOT NULL,
     "name" TEXT,
     "phone" TEXT,
-    "percentage" TEXT,
+    "score" TEXT,
     "gender" TEXT,
-    "birth_date" TEXT,
+    "birth_date" TIMESTAMP(3),
     "contribution_time" TEXT,
     "is_unhealthy" BOOLEAN,
     "is_military" BOOLEAN,
@@ -34,8 +37,27 @@ CREATE TABLE "Retired" (
     "utm_campaign" TEXT,
     "utm_content" TEXT,
     "utm_term" TEXT,
+    "ip" TEXT,
+    "device" TEXT,
+    "date_benefit_was_granted" TIMESTAMP(3),
+    "webhook_sent" BOOLEAN,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Retired_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "retired_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "request_logs" (
+    "id" SERIAL NOT NULL,
+    "method" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "headers" JSONB,
+    "body" JSONB,
+    "query_params" JSONB,
+    "ip" TEXT,
+    "user_agent" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "request_logs_pkey" PRIMARY KEY ("id")
 );
