95320c7ded1624170d3559cb4b7d1beb84630142
Leonardo Hentschke <leo.hentschke@hotmail.com>
2025-07-14 21:41:13 -0300

    update code

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,8 +5,6 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
-  DIRECT_URL: z.string().url(),
-  API_TOKEN: z.string().default("zns-secret-token-2025"),
   WEBHOOK_RETIRED_URL: z.string().url(),
   WEBHOOK_ATHLETES_RIGHTS_URL: z.string().url(),
 });
